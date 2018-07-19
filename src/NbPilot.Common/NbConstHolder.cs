dd362b33436be1886706011f7aa6cd22a296e423
congzw <2981810300@qq.com>
2018-07-19 15:15:44 +0800

    add NbConstHolder & specs

---
+++ src/NbPilot.Common/NbConstHolder.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NbPilot.Common
+{
+    /// <summary>
+    /// 实例容器（单例）
+    /// </summary>
+    public class NbConstHolder
+    {
+        /// <summary>
+        /// 实例容器（单例）
+        /// </summary>
+        public NbConstHolder()
+        {
+            Items = new Dictionary<Type, object>();
+        }
+
+        /// <summary>
+        /// 所有注册的项
+        /// </summary>
+        public Dictionary<Type, object> Items { get; set; }
+
+        /// <summary>
+        /// 获取
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public T Get<T>() where T : new()
+        {
+            var type = typeof (T);
+            if (!Items.ContainsKey(type))
+            {
+                Items[type] = new T();
+            }
+            return (T)Items[type];
+        }
+
+        /// <summary>
+        /// 设置
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        public void Set<T>(T value) where T : new()
+        {
+            var type = typeof(T);
+            Items[type] = value;
+        }
+
+        #region instance
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        static NbConstHolder()
+        {
+            Instance = new NbConstHolder();
+        }
+
+        /// <summary>
+        /// Singleton
+        /// </summary>
+        public static NbConstHolder Instance { get; set; }
+
+        #endregion
+    }
+}
---
+++ src/test/unit/NbPilot.Common.UnitTest/NbConstHolderSpec.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NbPilot.Common
+{
+    [TestClass]
+    public class NbConstHolderSpec
+    {
+        [TestMethod]
+        public void SetAndGet_Should_OK()
+        {
+            var instanceHolder = new NbConstHolder();
+            var mockItem = new MockHolderItem();
+            instanceHolder.Set(mockItem);
+
+            var holderMockItem = instanceHolder.Get<MockHolderItem>();
+            holderMockItem.ShouldNotNull("holderMockItem");
+            holderMockItem.ShouldSame(mockItem);
+            
+            var holderMockItem2 = instanceHolder.Get<MockHolderItem>();
+            holderMockItem2.ShouldSame(mockItem);
+        }
+
+        [TestMethod]
+        public void NotSetAndGet_Should_OK()
+        {
+            var instanceHolder = new NbConstHolder();
+            var mockHolderItemAuto = instanceHolder.Get<MockHolderItemB>();
+            mockHolderItemAuto.ShouldNotNull("mockHolderItemAuto");
+
+            var mockHolderItemAuto2 = instanceHolder.Get<MockHolderItemB>();
+            mockHolderItemAuto2.ShouldSame(mockHolderItemAuto);
+        }
+    }
+
+    #region mocks
+    
+    public class MockHolderItem
+    {
+        public string Foo = "Foo";
+    }
+
+    public class MockHolderItemB
+    {
+        public string Foo = "Foo";
+    }
+
+    #endregion
+}
